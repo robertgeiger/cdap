d2836ec12f8bf4b9836080dd7b386c7f80057969
poorna <poorna@cask.co>
2015-09-10 14:21:07 -0700

    WIP

--- cdap-data-fabric/src/test/java/co/cask/cdap/data2/metadata/lineage/LineageServiceTest.java
+++ cdap-data-fabric/src/test/java/co/cask/cdap/data2/metadata/lineage/LineageServiceTest.java
@@ -288,23 +288,25 @@ public class LineageServiceTest {
       ),
       lineage.getRelations());
 
-    // Lineage for D2 should be D1 -> P1 -> D2
+    // Lineage for D3 should be D1 -> P1 -> D2 -> P2 -> D3
     //                                |
     //                                |
     //                           D0 ->|
     //
-    lineage = lineageService.computeLineage(dataset2, 500, 20000, 100);
+    lineage = lineageService.computeLineage(dataset3, 500, 20000, 100);
     Assert.assertNotNull(lineage);
 
-    Assert.assertEquals(ImmutableSet.of(program1), lineage.getPrograms());
-    Assert.assertEquals(ImmutableSet.of(dataset0, dataset1, dataset2), lineage.getData());
+    Assert.assertEquals(ImmutableSet.of(program1, program2), lineage.getPrograms());
+    Assert.assertEquals(ImmutableSet.of(dataset0, dataset1, dataset2, dataset3), lineage.getData());
 
     Assert.assertEquals(
       ImmutableSet.of(
         new Relation(dataset0, program1, AccessType.READ, toSet(twillRunId(run1)), toSet(flowlet1)),
         new Relation(dataset1, program1, AccessType.READ, toSet(twillRunId(run1)), toSet(flowlet1)),
-        new Relation(dataset2, program1, AccessType.WRITE, toSet(twillRunId(run1)), toSet(flowlet1))
-      ),
+        new Relation(dataset2, program1, AccessType.WRITE, toSet(twillRunId(run1)), toSet(flowlet1)),
+        new Relation(dataset2, program2, AccessType.READ, toSet(twillRunId(run2)), toSet(flowlet2)),
+        new Relation(dataset3, program2, AccessType.WRITE, toSet(twillRunId(run2)), toSet(flowlet2))
+        ),
       lineage.getRelations());
   }
 
